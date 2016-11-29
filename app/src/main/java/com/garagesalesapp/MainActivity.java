1e52d80fecc8fcc6785559b53fb40875fda68668
Patrick Bailey <patrickbailey13@gmail.com>
2016-11-29 11:53:00 -0800

    added some code to grab info from a library and display it

--- app/src/main/java/com/garagesalesapp/MainActivity.java
+++ app/src/main/java/com/garagesalesapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.garagesalesapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.garagesaleslibrary.event.service.SaleEventManager;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int eventCount = getEventCount();
+
+        TextView tv = (TextView) findViewById(R.id.textView);
+
+        String displayText = "Number of events: " + eventCount;
+        tv.setText(displayText);
+    }
+    private int getEventCount(){
+        return SaleEventManager.getAllEvents(this).size();
     }
 }
