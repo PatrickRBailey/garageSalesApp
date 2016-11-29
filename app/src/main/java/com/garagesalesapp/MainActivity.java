1b50d234fe46cf00fe9c7824032f24c8cfecd2eb
Patrick Bailey <patrickbailey13@gmail.com>
2016-11-29 14:42:45 -0800

    custom layout adapter

--- app/src/main/java/com/garagesalesapp/MainActivity.java
+++ app/src/main/java/com/garagesalesapp/MainActivity.java
@@ -2,9 +2,13 @@ package com.garagesalesapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.garagesaleslibrary.event.domain.SaleEvent;
 import com.garagesaleslibrary.event.service.SaleEventManager;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,8 +24,21 @@ public class MainActivity extends AppCompatActivity {
 
         String displayText = "Number of events: " + eventCount;
         tv.setText(displayText);
+        displayListView();
     }
     private int getEventCount(){
         return SaleEventManager.getAllEvents(this).size();
+    }
+    private void displayListView(){
+        List<SaleEvent> events = SaleEventManager.getAllEvents(this);
+
+
+
+        final SaleEventArrayAdapter adapter =
+                new SaleEventArrayAdapter(this, R.layout.event_list_item,events);
+
+        ListView list = (ListView)findViewById(R.id.eventlistview);
+
+        list.setAdapter(adapter);
     }
 }
