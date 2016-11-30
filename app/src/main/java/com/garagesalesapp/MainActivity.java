4f9dbe257e2a1ecdeb597d7c2a96c53c93919b03
Patrick Bailey <patrickbailey13@gmail.com>
2016-11-29 16:33:38 -0800

    setup menu

--- app/src/main/java/com/garagesalesapp/MainActivity.java
+++ app/src/main/java/com/garagesalesapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.garagesalesapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -40,5 +42,38 @@ public class MainActivity extends AppCompatActivity {
         ListView list = (ListView)findViewById(R.id.eventlistview);
 
         list.setAdapter(adapter);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+// Handle item selection
+        switch (item.getItemId()) {
+            case R.id.mi_add_event:
+                addEvent();
+                return true;
+            case R.id.mi_show_map:
+                showMap();
+                return true;
+            case R.id.mi_prefs:
+                showPrefs();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    private void showPrefs() {
+
+    }
+    private void showMap() {
+
+    }
+    private void addEvent() {
+
     }
 }
